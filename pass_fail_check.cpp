fa7b4e47b427e6d1b3610016933de7c05c61a972
Ishtiaque Fahim <168330497+ishtiaque-fahim@users.noreply.github.com>
2024-11-19 20:34:08 +0600

    Create pass_fail_check.cpp

---
+++ pass_fail_check.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mark;
+    cout << "Enter Marks : ";
+    cin >> mark;
+    
+    if (mark >= 33)
+    {
+        cout<< "Passed";
+    }
+    else 
+    {        
+    cout<< "Failed";
+    }
+    return 0;
+}
