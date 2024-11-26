4e8a1ba680902571889e81e43f388d3ae54553df
Ishtiaque Fahim <168330497+ishtiaque-fahim@users.noreply.github.com>
2024-11-26 13:16:56 +0600

    Add files via upload

---
+++ Conditional or Ternary Operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, max;
+    cin>> num1 >> num2;
+    max = (num1>num2) ? num1:num2;
+    cout << max;
+
+    return 0;
+}
